48c6ce2668dec0bac3e66aaa3eb9e61a32009911
YVON <YVON@THINKPAD-X1>
2020-08-24 13:53:39 +0800

    middle commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyADe1kAP6g_o2mwnosxFfCPt-NMEIuEIhY",
+    authDomain: "clone-bca0d.firebaseapp.com",
+    databaseURL: "https://clone-bca0d.firebaseio.com",
+    projectId: "clone-bca0d",
+    storageBucket: "clone-bca0d.appspot.com",
+    messagingSenderId: "870203568792",
+    appId: "1:870203568792:web:9980952291ba08cc7b56ed",
+    measurementId: "G-X998RWCFDF"
+};
